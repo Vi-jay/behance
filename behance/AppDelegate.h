3b2413b5d88a033cb93a0179889335f07b762b3d
Vijay <765139628@qq.com>
2017-10-22 23:05:20 +0800

    Initial Commit

---
+++ behance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  behance
+//
+//  Created by 王慧 on 2017/10/22.
+//  Copyright © 2017年 王慧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ behance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  behance
+//
+//  Created by 王慧 on 2017/10/22.
+//  Copyright © 2017年 王慧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
